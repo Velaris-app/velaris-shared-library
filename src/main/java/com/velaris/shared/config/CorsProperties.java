ea8ec4274732cac7ff711049f7f096ffc227251a
BashEbash <pidppid@gmail.com>
2025-10-08 23:24:36 +0200

    fix cors props

--- src/main/java/com/velaris/shared/config/CorsProperties.java
+++ src/main/java/com/velaris/shared/config/CorsProperties.java
@@ -1,19 +1,15 @@
 package com.velaris.shared.config;
 
-import lombok.Getter;
-import lombok.RequiredArgsConstructor;
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.boot.context.properties.bind.ConstructorBinding;
 import org.springframework.context.annotation.Configuration;
 import java.util.List;
 
 @Configuration
 @ConfigurationProperties(prefix = "cors")
-@Getter
-@RequiredArgsConstructor(onConstructor_ = @ConstructorBinding)
-public class CorsProperties {
-    private final List<String> allowedOrigins;
-    private final List<String> allowedMethods;
-    private final List<String> allowedHeaders;
-    private final boolean allowCredentials;
+public record CorsProperties(
+        List<String> allowedOrigins,
+        List<String> allowedMethods,
+        List<String> allowedHeaders,
+        Boolean allowCredentials
+) {
 }
