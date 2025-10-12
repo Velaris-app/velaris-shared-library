bcb820b54a638125100602b62380b7c343f80a75
BashEbash <pidppid@gmail.com>
2025-10-12 13:29:23 +0200

    fix super builder

--- src/main/java/com/velaris/shared/security/SecurityUserDetails.java
+++ src/main/java/com/velaris/shared/security/SecurityUserDetails.java
@@ -2,12 +2,43 @@ package com.velaris.shared.security;
 
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
-import org.springframework.security.core.userdetails.User;
 import lombok.experimental.SuperBuilder;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import java.util.Collection;
 
 @Getter
 @SuperBuilder
-@EqualsAndHashCode(callSuper = false)
-public class SecurityUserDetails extends User {
+@EqualsAndHashCode
+public class SecurityUserDetails implements UserDetails {
+
     private final Long id;
+    private final String username;
+    private final String password;
+    private final Collection<? extends GrantedAuthority> authorities;
+    private final boolean accountNonExpired;
+    private final boolean accountNonLocked;
+    private final boolean credentialsNonExpired;
+    private final boolean enabled;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() { return authorities; }
+
+    @Override
+    public String getPassword() { return password; }
+
+    @Override
+    public String getUsername() { return username; }
+
+    @Override
+    public boolean isAccountNonExpired() { return accountNonExpired; }
+
+    @Override
+    public boolean isAccountNonLocked() { return accountNonLocked; }
+
+    @Override
+    public boolean isCredentialsNonExpired() { return credentialsNonExpired; }
+
+    @Override
+    public boolean isEnabled() { return enabled; }
 }
